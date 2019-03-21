3fdc5a417a1f546833109d9190d5525b183f3f7b
nitheeshkm <n67@umbc.edu>
2019-03-21 13:44:08 -0400

    adding sh and bat files

---
+++ git.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd "C:\Users\nitheesh\Desktop\RA\TWTFT\Git-Collab Project\fpga_controller_for_synchronization_of_distant_clocks"
+git status
+read -p "Press enter to pull repository"
+git pull
+read -p "Press enter to exit"
+read -p "Press enter again to confim exit"
+
+
---
+++ update_twtft.bat
@@ -0,0 +1,5 @@
+#C:
+#cd \Users\nitheesh\Desktop\RA\TWTFT\Git-collab Project\fpga_controller_for_synchronization_of_distant_clocks
+#start "" "%SYSTEMDRIVE%\Program Files\Git\bin\sh.exe" --login -i -c "\Users\nitheesh\Desktop\test_git.sh"
+
+"c:\Program Files\Git\bin\sh.exe" --login -i -c "/c/Users/nitheesh/Desktop/git.sh"
